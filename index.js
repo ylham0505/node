e9c951bf31ff9a6e746552f46100444c7484579d
ylham0505 <serdar.garyagdyyew204@gmail.com>
2023-10-19 23:22:03 +0500

    2.0.0

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require('express') // wyzywaem express.js
 const mongoose = require('mongoose')
 const PORT = process.env.PORT || 3000 //nastraiwaem port default(localhost:3000)
+const exphbs = require('express-handlebars')
+const todoRoutes = require('./routes/todos')
+const path = require('path')
 
 const app = express() //obyekt prilojeniya mojno wyzywat razlicnye funkciyi
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname,'public')))
+
+
+app.use(todoRoutes)
+
+
 
 async function start() {
     try {
